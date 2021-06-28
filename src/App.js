a2031a7d94ea794c538ac1241c218d44ec1649f3
Yifang Ma <yifang@nearprotocol.com>
2021-06-28 16:56:22 +0800

    first build

--- src/App.js
+++ src/App.js
@@ -1,24 +1,79 @@
-import logo from './logo.svg';
+import React, { useState } from 'react';
+import { Button, Table, Row, Container } from 'react-bootstrap';
 import './App.css';
 
+import Category from './data/category-list.json';
+import Partner from './data/partner-list.json';
+
 function App() {
+  const [cat, setC] = useState('');
+  console.log(cat);
+  const Tr = ({ num, logo, title, link, categories, cat }) => {
+    console.log(title, categories.includes(cat));
+    if (cat === '' || categories.includes(cat)) {
+      return (
+        <tr key={num} className="table-item">
+          <td>{num}</td>
+          <td>
+            <div>
+              <img src={logo} alt="" className="logo" />
+              <a
+                href={link}
+                target="_blank"
+                rel="noreferrer"
+                style={{ textDecoration: 'none', color: '#A45C40' }}
+              >
+                <strong style={{ fontSize: '1.2rem' }}>{title}</strong>
+              </a>
+            </div>
+          </td>
+          <td>{categories.join(', ')}</td>
+          <td>0</td>
+          <td>0</td>
+          <td>0</td>
+        </tr>
+      );
+    }
+    return <></>;
+  };
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
+    <Container>
+      <h1>NEAR Dapps</h1>
+      <Row noGutters>
+        <Button onClick={() => setC('')}>All</Button>
+        {Category.map((c) => (
+          <Button key={c.title} onClick={() => setC(c.title)}>
+            {c.title} {c.count}
+          </Button>
+        ))}
+      </Row>
+      <Row noGutters>
+        <Table>
+          <thead>
+            <tr>
+              <th>#</th>
+              <th>Title</th>
+              <th>Category</th>
+              <th>Users</th>
+              <th>Balance</th>
+              <th>Transactions</th>
+            </tr>
+          </thead>
+          <tbody>
+            {Partner.map((p, i) => (
+              <Tr
+                num={i + 1}
+                logo={p.logo}
+                title={p.title}
+                link={p.link}
+                categories={p.categories}
+                cat={cat}
+              />
+            ))}
+          </tbody>
+        </Table>
+      </Row>
+    </Container>
   );
 }
 
